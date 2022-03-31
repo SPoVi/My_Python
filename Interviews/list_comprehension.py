3972a871b3245b3c8b2994ad12816a12031b5ca7
S.Pons <snop.vlc@gmail.com>
2022-03-31 12:31:07 +0200

    Fix - list_comprehension.py

--- Interviews/list_comprehension.py
+++ Interviews/list_comprehension.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 
 '''You are given three integers 'x,y,z' and representing the dimensions of a cuboid along with an integer 'n' . 
 Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of 'i+j+k'is not equal to 'n'.
